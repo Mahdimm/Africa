68e352a7727472ff072be160425f22d5428ea9fc
Woocer <woocer.app@gmail.com>
2021-04-19 16:06:54 +0430

    Initial Commit

---
+++ Africa/AfricaApp.swift
@@ -0,0 +1,17 @@
+//
+//  AfricaApp.swift
+//  Africa
+//
+//  Created by Mahdi Mahjoobi on 4/19/21.
+//
+
+import SwiftUI
+
+@main
+struct AfricaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
